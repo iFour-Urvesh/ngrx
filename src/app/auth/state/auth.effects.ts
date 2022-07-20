1232780e072e9c08f870b8a12ca1931fa440411d
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-07-20 11:20:05 +0530

    save user token from login

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -10,9 +10,10 @@ export class AuthEffects {
     constructor( private action$ : Actions, private authService : AuthService) {}
 
     login$ = createEffect(() => {
-        return this.action$.pipe(ofType(loginStart), exhaustMap((action) => {
+        return this.action$.pipe(ofType(loginStart), exhaustMap((action) => { 
             return this.authService.login(action.email, action.password).pipe(map((data) => {
-                return loginSuccess();
+                const user = this.authService.formatUser(data);
+                return loginSuccess({user});
             }))
         }))
     })
